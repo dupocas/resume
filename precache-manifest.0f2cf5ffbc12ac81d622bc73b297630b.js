34e231a4fef4c643125b3fb8b4ada98fc36a2abd
Eduardo <dupocas@gmail.com>
2020-10-18 20:48:52 -0300

    Updates

---
+++ precache-manifest.0f2cf5ffbc12ac81d622bc73b297630b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803f1466389867cf1268752aaf519ff9",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "639d43178f34a416d0d6",
+    "url": "/resume/static/css/main.038695f1.chunk.css"
+  },
+  {
+    "revision": "9808678dce30667fc381",
+    "url": "/resume/static/js/2.c60ef868.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/resume/static/js/2.c60ef868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639d43178f34a416d0d6",
+    "url": "/resume/static/js/main.23495050.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  }
+]);
